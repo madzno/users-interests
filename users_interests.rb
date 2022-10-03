7e00956257b1ff92f50678003d490baaf17b8829
Madz No <madzno12@gmail.com>
2022-10-03 15:51:55 -0400

    refractoring code

--- users_interests.rb
+++ users_interests.rb
@@ -1,26 +1,37 @@
+require 'yaml'
+
 require 'tilt/erubis'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'yaml'
+
+before do
+  @users = YAML.load_file('users.yml')
+end
+
+helpers do
+  def count_interests
+    interests_counts = []
+
+    @users.values.each { |hash| interests_counts << (hash[:interests].count) }
+
+   interests_counts.sum
+  end
+end
 
 get '/' do
-  users = YAML.load_file('users.yml')
 
   @names = []
-
-  users.each_key { |key| @names << key }
+  @users.each_key { |key| @names << key }
 
   erb :home
 end
 
 get '/:name' do
-  users = YAML.load_file('users.yml')
-  name = params[:name].to_sym
+  @user_name = params[:name].to_sym
+  @email = @users[@user_name][:email]
+  @interests = @users[@user_name][:interests].join(', ')
 
-  users_info = users[name]
+  @other_names = @users.keys.select { |other_user| other_user != @name }
 
-  @email = users_info[:email]
-  @interests = users_info[:interests]
-
-  erb :name
+  erb :user_page
 end
